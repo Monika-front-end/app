0dfdc877472d4f0340a57a9e030bf29cbed9eb19
Monika Ludwikowska <monikaagaludwikowska79@gmail.com>
2020-02-11 18:02:01 +0100

    Updates

---
+++ precache-manifest.aece416d8d82e0fb2c7be304716a1197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a13d011eae8a923d21124322177b97",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "fc6d6632f178ecd24499",
+    "url": "/app/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "7ade2966325f252ff961",
+    "url": "/app/static/js/2.e633860e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/app/static/js/2.e633860e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6d6632f178ecd24499",
+    "url": "/app/static/js/main.03f8f17b.chunk.js"
+  },
+  {
+    "revision": "36ac6f6e7e09128690e6",
+    "url": "/app/static/js/runtime-main.0338d7e7.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/app/static/media/logo.ee7cd8ed.svg"
+  }
+]);
